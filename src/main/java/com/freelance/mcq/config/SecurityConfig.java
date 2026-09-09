1c45d02993bade269dda91a0bacfa3aacbc3ee02
gajun755 <gajun755@gmail.com>
2026-09-09 13:36:35 +0530

    health check added and changed securityconfig

--- src/main/java/com/freelance/mcq/config/SecurityConfig.java
+++ src/main/java/com/freelance/mcq/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 })
             )
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/**","/api/health").permitAll()
                 //.requestMatchers("/api/admin/**").permitAll()
                 .anyRequest().authenticated()
             )
